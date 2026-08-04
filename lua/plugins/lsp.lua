8b5728830448b4aad56c28a0d121f7dc4a1a36de
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-08-04 10:54:48 -0300

    feat: add conform for code format

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,12 +4,12 @@ return {
     version = 'v2.*',
     dependencies = {
       'saghen/blink.cmp',
-      'mason-org/mason.nvim'
+      'mason-org/mason.nvim',
     },
     event = 'VeryLazy',
     config = function()
-      require('plugins.config.lsp')
-    end
+      require 'plugins.config.lsp'
+    end,
   },
   {
     'saghen/blink.cmp',
@@ -24,9 +24,9 @@ return {
           draw = {
             columns = {
               { 'label', 'label_description', gap = 1 },
-              { 'kind_icon', 'kind', gap = 1 }
-            }
-          }
+              { 'kind_icon', 'kind', gap = 1 },
+            },
+          },
         },
         documentation = {
           auto_show = true,
@@ -39,35 +39,75 @@ return {
         ['<C-\\>'] = { 'show' },
         ['<C-e>'] = { 'hide' },
         ['<CR>'] = { 'accept', 'fallback' },
-      }
-    }
+      },
+    },
   },
   {
     'mason-org/mason.nvim',
     version = 'v2.*',
     dependencies = 'mason-org/mason-lspconfig.nvim',
     keys = {
-      {'<leader>m', '<cmd>Mason<CR>', 'Mason Menu'},
+      { '<leader>m', '<cmd>Mason<CR>', 'Mason Menu' },
     },
     config = function()
-      require('plugins.config.mason')
-    end
+      require 'plugins.config.mason'
+    end,
   },
   {
     'L3MON4D3/LuaSnip',
     version = 'v2.*',
     dependencies = 'rafamadriz/friendly-snippets',
     lazy = true,
-    config = function ()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
+    end,
+  },
+  {
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true }
+        end,
+        mode = '',
+        desc = 'Format buffer',
+      },
+    },
+    ---@module 'conform'
+    ---@type conform.setupOpts
+    opts = {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        sh = { 'shfmt' },
+        python = { 'ruff' },
+        toml = { 'taplo' },
+        yaml = { 'yamlfmt' },
+        ansible = { 'yamlfmt' },
+      },
+      default_format_opts = {
+        lsp_format = 'fallback',
+      },
+      format_on_save = {
+        timeout_ms = 500,
+      },
+    },
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
   },
   {
     'folke/trouble.nvim',
     version = 'v3.*',
     keys = {
       { '<leader>tx', '<cmd>Trouble diagnostics toggle<CR>', 'Diagnostics' },
-      { '<leader>tX', '<cmd>Trouble diagnostics toggle filter.buf=0<CR>', 'Buffer Diagnostics' },
+      {
+        '<leader>tX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<CR>',
+        'Buffer Diagnostics',
+      },
       { 'td', '<cmd>Trouble lsp_definitions toggle<CR>', 'Diagnostics' },
     },
     opts = {
@@ -76,10 +116,10 @@ return {
         lsp_definitions = {
           win = {
             position = 'right',
-            size = 80
+            size = 80,
           },
         },
-      }
+      },
     },
     config = function(_, opts)
       local has_ts, trouble_ts = pcall(require, 'trouble.view.treesitter')
@@ -87,6 +127,6 @@ return {
         trouble_ts.setup = function() end
       end
       require('trouble').setup(opts)
-    end
-  }
+    end,
+  },
 }

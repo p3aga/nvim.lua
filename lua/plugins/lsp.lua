7acf57ece24eee0d9ea967b512eb5f4a6b520db5
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-08-10 16:27:02 -0300

    feat: setup LuaSnip jsregexp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -57,6 +57,7 @@ return {
     'L3MON4D3/LuaSnip',
     version = 'v2.*',
     dependencies = 'rafamadriz/friendly-snippets',
+    build = 'make install_jsregexp',
     lazy = true,
     config = function()
       require('luasnip.loaders.from_vscode').lazy_load()
